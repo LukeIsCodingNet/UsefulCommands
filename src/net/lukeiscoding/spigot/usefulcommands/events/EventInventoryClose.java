66406a1d93f75cb5eca0f78e6f19c1c325d69ffd
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 10:40:44 -0500

    first commit

---
+++ src/net/lukeiscoding/spigot/usefulcommands/events/EventInventoryClose.java
@@ -0,0 +1,13 @@
+package net.lukeiscoding.spigot.usefulcommands.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class EventInventoryClose implements Listener {
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+    }
+}
