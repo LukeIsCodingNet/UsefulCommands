d7dcf0a18f744acba5da2347ea77fe57463b4771
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 10:44:01 -0500

    Updated some things.

--- src/net/lukeiscoding/spigot/usefulcommands/events/MOTDMessageListener.java
+++ src/net/lukeiscoding/spigot/usefulcommands/events/MOTDMessageListener.java
@@ -2,6 +2,7 @@ package net.lukeiscoding.spigot.usefulcommands.events;
 
 import net.lukeiscoding.spigot.usefulcommands.ConfigManager;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -9,6 +10,7 @@ public class MOTDMessageListener implements Listener {
 
     private static final ConfigManager configManager = new ConfigManager();
 
+    @EventHandler
     public void sendMOTD(PlayerJoinEvent event) {
         Player p = event.getPlayer();
 
