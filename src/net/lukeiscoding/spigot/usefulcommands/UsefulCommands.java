b5233c9b696e62390fdd78112a1fbad96a4ae170
LukeIsCodingNet <lukeiscoding@gmail.com>
2021-01-10 12:52:30 -0500

    Added new commands and fixed the issue where the events where not being registered

--- src/net/lukeiscoding/spigot/usefulcommands/UsefulCommands.java
+++ src/net/lukeiscoding/spigot/usefulcommands/UsefulCommands.java
@@ -1,14 +1,21 @@
 package net.lukeiscoding.spigot.usefulcommands;
 
+import net.lukeiscoding.spigot.usefulcommands.commandcompleation.TeleportCommandCompleter;
 import net.lukeiscoding.spigot.usefulcommands.commands.*;
+import net.lukeiscoding.spigot.usefulcommands.events.EventInventoryClose;
 import net.lukeiscoding.spigot.usefulcommands.events.MOTDMessageListener;
+import net.lukeiscoding.spigot.usefulcommands.events.PlayerSignInteractEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UsefulCommands extends JavaPlugin {
+
+    /**
+     * @name Useful Commands
+     * @author Luke Is Coding
+     * This project is lisensed under GNU General Public License version 2.0 or later. See LICENSE.md
+     */
 
     private ConfigManager configManager;
 
@@ -43,17 +50,18 @@ public class UsefulCommands extends JavaPlugin {
     private void registerCommands() {
         this.getCommand("clearchat").setExecutor(new CommandClearChat());
         this.getCommand("testgui").setExecutor(new CommandTestGUI());
-        this.getCommand("usefulcommands").setExecutor(new CommandUsefulCommands());
         this.getCommand("destoryitems").setExecutor(new CommandDestroyItems());
         this.getCommand("crafting").setExecutor(new CommandCrafting());
         this.getCommand("fly").setExecutor(new CommandFly());
         this.getCommand("smelt").setExecutor(new CommandSmelt());
+        this.getCommand("nick").setExecutor(new CommandNickname());
+        this.getCommand("destroy").setExecutor(new CommandDestroyItem());
     }
 
-    // TODO: Fix events not registering
     private void registerEvents() {
-        final PluginManager pm = Bukkit.getServer().getPluginManager();
-        pm.registerEvents(new MOTDMessageListener(), this);
+        this.getServer().getPluginManager().registerEvents(new MOTDMessageListener(), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerSignInteractEvent(), this);
+        this.getServer().getPluginManager().registerEvents(new EventInventoryClose(), this);
     }
 
     private void loadConfigManager() {
@@ -66,9 +74,12 @@ public class UsefulCommands extends JavaPlugin {
     }
 
     private void setConfigDefaults() {
+        // config defaults for messages.yml
         configManager.getMessagesConfig().addDefault("player-join-message", "&bWelcome &a%PLAYER% to the server!");
         configManager.getMessagesConfig().addDefault("crafting-table-command-message", "&bCrafting");
         configManager.getMessagesConfig().addDefault("furnace-command-message", "&bSmelting...");
         configManager.getMessagesConfig().addDefault("furnace-gui-title", "'Furnace'");
+
+        // config defaults for kits.yml
     }
 }
