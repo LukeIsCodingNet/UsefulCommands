2dbe81f77b00e1d80b92f246130ea2cb48b0ba55
LukeIsCodingNet <lukeiscoding@gmail.com>
2021-01-10 12:49:02 -0500

    fixed some minor bugs

--- src/net/lukeiscoding/spigot/usefulcommands/commands/CommandSmelt.java
+++ src/net/lukeiscoding/spigot/usefulcommands/commands/CommandSmelt.java
@@ -1,4 +1,5 @@
 package net.lukeiscoding.spigot.usefulcommands.commands;
+
 import net.lukeiscoding.spigot.usefulcommands.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -27,11 +28,32 @@ public class CommandSmelt implements CommandExecutor {
         // create the furnace inventory
         final Inventory furnace = Bukkit.createInventory(null, InventoryType.FURNACE, "Furnace");
 
+        // create the blast furnace inventory
+        final Inventory blastFurnace = Bukkit.createInventory(null, InventoryType.BLAST_FURNACE, "Blast Furnace");
+
         // check if the player has permission and check for command name
         if (p.hasPermission("usefulcommands.smelt")) {
             if (cmd.getName().equalsIgnoreCase("smelt")) {
-                p.openInventory(furnace);
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessagesConfig().getString("furnace-gui-title")));
+                // check if the player did not enter any arguments
+                if (args.length == 0) {
+                    p.sendMessage(ChatColor.RED + "You did not enter any arguments see /smelt help");
+                }
+
+                if (args[1].equalsIgnoreCase("help")) {
+                    p.sendMessage(ChatColor.AQUA + "Smelt command help:");
+                    p.sendMessage(ChatColor.AQUA + "/smelt furnace opens a furnace GUI.");
+                    p.sendMessage(ChatColor.AQUA + "/smelt blastFurnace opens a black furnace GUI.");
+                }
+
+                if (args[1].equalsIgnoreCase("furnace")) {
+                    p.openInventory(furnace);
+                    p.sendMessage(ChatColor.AQUA + "Smelting...");
+                }
+
+                if (args[1].equalsIgnoreCase("blastFurnace")) {
+                    p.openInventory(blastFurnace);
+                    p.sendMessage(ChatColor.AQUA + "Blast smelting...");
+                }
             }
         }
 
